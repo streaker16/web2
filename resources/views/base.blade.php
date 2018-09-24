29cba2d88358be51615b25bd9055c117ef2e4569
streaker16 <abidaffa72@gmail.com>
2018-09-24 08:57:18 +0700

    halaman dua tiga

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -40,10 +40,10 @@
                 <a href="/">Home</a>
             </li>
             <li>
-                <a href="#">- Sidebar 1</a>
+                <a href="/halamandua">- Halamanan Dua</a>
             </li>
             <li>
-                <a href="#">- Sidebar 2</a>
+                <a href="halamantiga">- Halaman Tiga</a>
             </li>
         </ul>
     </aside>
